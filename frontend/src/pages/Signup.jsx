366af7966efbe003a399e230015671772fffebf1
Sadiq Gurramkonda <gurramkondasadiq216@gmail.com>
2024-08-28 21:29:26 +0530

    some modifications

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -19,8 +19,8 @@ export default function Signup(){
 
     return (
         <div className="bg-slate-300 h-screen flex justify-center">
-            <div className='flex flex-col justify-center'>
-                <div className='rounded-lg bg-white w-80 text-center p-2 h-max px-4 '>
+            <div className='flex flex-col justify-center' >
+                <div className='rounded-lg bg-white text-center p-2 h-max px-4 '>
                     <Heading label={"Sign up"}></Heading>
                     <SubHeading label={"Enter your information to create an account"}></SubHeading>
                     <InputBox onChange={e => {
